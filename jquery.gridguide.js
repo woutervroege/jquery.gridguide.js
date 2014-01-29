0ecfd158e2d013be897902f757ad3f815d200a6b
Wouter Vroege <wouter@woutervroege.nl>
2014-01-29 12:24:30 +0100

    changes in settings
    removed total width; columnwidth and gutter autocalc the total width of the grid
    
    Signed-off-by: Wouter Vroege <wouter@woutervroege.nl>

--- jquery.gridguide.js
+++ jquery.gridguide.js
@@ -13,40 +13,32 @@
         var settings = $.extend({
             columns: 2,
             gutter: 10,
-            width: 800,
+            columnwidth: 800,
             bordercolor: "red",
             backgroundcolor: "rgba(255, 0, 0, .1)"
         }, options);
-        settings.colwidth = ((settings.width - ((settings.columns-1) * settings.gutter)) / settings.columns);
+        settings.gridwidth = settings.columns * settings.columnwidth + (settings.gutter * (settings.columns-1));
 
         var grid = $('<div class="grid"></div>');
         grid.css({
             "z-index": 10,
-            "width": settings.width + "px",
+            "width": settings.gridwidth + "px",
             "height": $(window).height() + "px",
             "position": "fixed",
             "left": "50%",
             "top": "0",
-            "margin-left": "-" + ((settings.width / 2)+2) + "px",
-            "border-color": settings.bordercolor,
-            "border-style": "solid",
-            "border-left-width": "1px",
-            "border-right-width": "1px",
-            "border-top-width": "0",
-            "border-bottom-width": "0",
+            "margin-left": "-" + (settings.gridwidth / 2) + "px"
         })
         $(ELEM).append($(grid));
         for(var i=0;i<settings.columns;i++) {
             $(grid).append(
                 $('<div></div>').css({
-                    "width": settings.colwidth + "px",
+                    "width": settings.columnwidth + "px",
                     "height": "100%",
                     "display": "inline-block",
                     "position": "absolute",
-                    "left": ((settings.colwidth + settings.gutter) * i) + "px",
+                    "left": ((settings.columnwidth + settings.gutter) * i) + "px",
                     "vertical-align": "top",
-                    "border-left": (i==0) ? 0 : "1px solid " + settings.bordercolor,
-                    "border-right": (i==settings.columns-1) ? 0 : "1px solid " + settings.bordercolor,
                     "background": settings.backgroundcolor
                 })
             )
